0717e8d8b939376b4af029746c2daa04c26e14f8
Theeraj Subhakaar <theerajsubhakaar@gmail.com>
2024-12-23 21:20:48 +0530

    updates file name changer.py

--- file_name_change.py
+++ file_name_change.py
@@ -1,10 +1,13 @@
-import os 
+import os
+def rename_files_with_spaces(directory):
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            if ' ' in filename:
+                new_filename = filename.replace(' ', '_')
+                old_file = os.path.join(root, filename)
+                new_file = os.path.join(root, new_filename)
+                os.rename(old_file, new_file)
+                print(f"Renamed: {old_file} -> {new_file}")
 
-path = "Multiple_dataset"
-dis_list = os.listdir(path)
-for i in dis_list:
-    new_path = str(path)+"/"+str(i)
-    dis_list1 = os.listdir(new_path)
-    for j in dis_list1:
-        new_path1 = new_path+"/"+str(j)
-        print(new_path1)
+directory_path = "/path/to/your/directory"
+rename_files_with_spaces(directory_path)
